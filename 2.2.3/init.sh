9cbf2cbf3c4458624596cd669eff1b64714d36ea
João Loff <jfloff@gmail.com>
2017-02-26 13:05:49 +0000

    Add to entrypoint script to check if bundle update or migrations are pending

--- 2.2.3/init.sh
+++ 2.2.3/init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# if any changes to Gemfile occur between runs (e.g. if you mounted the
+# host directory in the container), it will install changes before proceeding
+bundle check || bundle install
+
+# Runs pending migrations if any pending
+if rake db:migrate:status | grep -q '  down'; then
+  rake db:migrate
+fi
+
 if [ $RAILS_ENV == "production" ]; then
   bundle exec rake assets:precompile
 fi
